5192b626e8508a0b1178c2a3accaf6d0966654a5
ComradeVanti <kingvanti650@gmail.com>
2022-07-07 09:21:48 +0200

    feat: export surface transform

--- Sources/RoomPlan.JsonExport/Convert.swift
+++ Sources/RoomPlan.JsonExport/Convert.swift
@@ -8,17 +8,22 @@ fileprivate struct V3Model : Encodable {
     let z: Float
 }
 
+fileprivate typealias MatrixModel = [Float]
+
 fileprivate class GenericSurfaceModel : Encodable {
     let category: String
     let id: String
     let confidence: String
     let scale: V3Model
+    let transform: MatrixModel
     
-    init(_ category: String, _ id: String, _ confidence: String, _ scale: V3Model) {
+    init(_ category: String, _ id: String, _ confidence: String,
+         _ scale: V3Model, _ transform: MatrixModel) {
         self.category = category
         self.id = id
         self.confidence = confidence
         self.scale = scale
+        self.transform = transform
     }
 }
 
@@ -43,22 +48,33 @@ fileprivate func toModel (_ v3: simd_float3) -> V3Model {
     V3Model(x: v3.x, y: v3.y, z: v3.z)
 }
 
+fileprivate func toModel (_ matrix: simd_float4x4) -> MatrixModel {
+    let (c1, c2, c3, c4) = matrix.columns
+    return [
+        c1.x, c1.y, c1.z, c1.w,
+        c2.x, c2.y, c2.z, c2.w,
+        c3.x, c3.y, c3.z, c3.w,
+        c4.x, c4.y, c4.z, c4.w
+    ]
+}
+
 fileprivate func toModel(_ surface: CapturedRoom.Surface) -> GenericSurfaceModel {
     let id = toModel(surface.identifier)
     let confidence = toModel(surface.confidence)
     let scale = toModel(surface.dimensions)
+    let transform = toModel(surface.transform)
     
     switch surface.category {
     case .door:
-        return GenericSurfaceModel("door", id, confidence, scale)
+        return GenericSurfaceModel("door", id, confidence, scale, transform)
     case .opening:
-        return GenericSurfaceModel("opening", id, confidence, scale)
+        return GenericSurfaceModel("opening", id, confidence, scale, transform)
     case .wall:
-        return GenericSurfaceModel("wall", id, confidence, scale)
+        return GenericSurfaceModel("wall", id, confidence, scale, transform)
     case .window:
-        return GenericSurfaceModel("window", id, confidence, scale)
+        return GenericSurfaceModel("window", id, confidence, scale, transform)
     default:
-        return GenericSurfaceModel("unknown", id, confidence, scale)
+        return GenericSurfaceModel("unknown", id, confidence, scale, transform)
     }
 }
 
